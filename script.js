2d8137c690e49cf8fca2facad0ea6af72606294b
Jose G. Herrera <herrjoseg@gmail.com>
2023-06-05 21:47:28 -0500

    Commit-VariablesScript
    Se añaden datos con variables en el Footer, para no tener que repetir en capa pagina .html, sino en la de script.js y se actualice en todas las paginas .html

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const ptitleAboutMe = document.getElementById("p-title-about-me")
+const pdatosAboutMe = document.getElementById("p-about-me")
+
+let nombre = "Jose G. Herrera";
+let ocupacion ="Estudiante de Full Stack en Egg Cooperation";
+let correo = "mi_correo@gmail.com";
+let telefono = "300-000-0000";
+let ubicacion = "Barranquilla - Colombia";
+
+ptitleAboutMe.innerHTML = nombre;
+pdatosAboutMe.innerHTML = `
+${ocupacion}<br>
+${correo}<br>
+${telefono}<br>
+${ubicacion}
+`;
+
 function showAlert() {
     alert('Gracias por suscribirse');
 }
